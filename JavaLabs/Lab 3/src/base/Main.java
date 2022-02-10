8560b36e5b5abe58e2d0334b1acba44afd5c1760
andrewdoherty1 <Andrew.Doherty1@marist.edu>
2022-02-10 17:24:08 -0500

    added squiggle brackets to Main
    Forgot squiggle brackets even though the code worked without it, I added them in.

--- JavaLabs/Lab 3/src/base/Main.java
+++ JavaLabs/Lab 3/src/base/Main.java
@@ -23,28 +23,38 @@ public class Main {
         int x = 5;
         int y = 3;
         //1st
-        if (x == 5)
+        if (x == 5){
             System.out.println("x equals 5");
+        }
         //2nd
-        if (x == 3)
+        if (x == 3){
             System.out.println("x equals 3");
-        else
+        }
+    
+        else{
             System.out.println("x does not equal 3");
+        }
+
         //3rd
-        if (x == 3)
+        if (x == 3){
             System.out.println("x equals 3");
+        }
         else
-        if(x == 5)
+        if(x == 5){
             System.out.println("x equals 5");
+        }
         //4th
-        if (x == 5)
+        if (x == 5){
             System.out.println("The value of y is " + y);
+        }
         //5th
-        if (x == 5 && y == 3)
+        if (x == 5 && y == 3){
             System.out.println("x equals 5 and y equals 3");
+        }
         //6th
-        if (x == 5 || y == 3)
+        if (x == 5 || y == 3){
             System.out.println("Either x equals 5 or y equals 3, or even both x equals 5 and y equals 3");
+        }
 
     }
 }
